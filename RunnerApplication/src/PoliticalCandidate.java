babfa97b8694067fb11a03a3e56168825b2b666e
Azin <48130819+azinmbd@users.noreply.github.com>
2023-05-16 17:20:03 -0700

    final application

---
+++ RunnerApplication/src/PoliticalCandidate.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Azinm
+ */
+class PoliticalCandidate implements Runner {
+    @Override
+    public void run() {
+        System.out.println("The political candidate is running for office.");
+    }
+}
